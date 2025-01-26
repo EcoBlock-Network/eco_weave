8883969109bd85ee8ffd9a014a938ff5974f5ab1
HenryMalo <winrhy@gmail.com>
2025-01-26 17:48:21 +0100

    refactor: :recycle: Move tests node.rs >> node_tests.rs

--- src/node.rs
+++ src/node.rs
@@ -23,30 +23,3 @@ impl Node {
         self.neighbors.contains(&neighbor_id.to_string())
     }
 }
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_node_creation() {
-        let node = Node::new("node1");
-        assert_eq!(node.id, "node1");
-        assert!(node.neighbors.is_empty());
-    }
-
-    #[test]
-    fn test_add_neighbor() {
-        let mut node = Node::new("node1");
-        node.add_neighbor("node2");
-        assert_eq!(node.neighbors.len(), 1);
-        assert!(node.is_neighbor("node2"));
-    }
-
-    #[test]
-    fn test_no_duplicate_neighbors() {
-        let mut node = Node::new("node1");
-        node.add_neighbor("node2");
-        node.add_neighbor("node2");
-        assert_eq!(node.neighbors.len(), 1);
-    }
-}
